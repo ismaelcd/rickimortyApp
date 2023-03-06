edc531f8a090af78e27c56f82e80a11f222818bc
ismaelcd <ismaelcd@gmail.com>
2023-03-06 12:06:32 +0100

    Componente del form terminado y enviando parametros a la url

--- rickimortyApp/src/app/shared/components/form-search/form-search.component.ts
+++ rickimortyApp/src/app/shared/components/form-search/form-search.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-form-search',
-  templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.scss']
+	selector: 'app-form-search',
+	template: `<input
+		#inputSearch
+		autofocus
+		type="text"
+		class="form-control-lg"
+		placeholder="Search..."
+		(keyup)="onSearch(inputSearch.value)"
+	/>`,
+	styles: ['input {width:100%}'],
 })
-export class FormSearchComponent {
+export class FormSearchComponent implements OnInit {
+	constructor(private router: Router) {}
 
+	ngOnInit(): void {}
+
+	/* escuchamos al form y lo pasamos como parametro a la url */
+	onSearch(value: string) {
+		console.log('Buscar=>', value);
+
+		if (value && value.length > 3) {
+			this.router.navigate(['/character-list'], {
+				queryParams: { q: value },
+			});
+		}
+	}
 }
